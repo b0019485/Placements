7bb6ed7978fa97e7053e866e1f3df8e9dbcae8e6
Zack Eaton <b001945@my.shu.ac.uk>
2012-05-13 14:14:15 +0100

    Storing the imported data for later use, using def (name)class =

--- Placements/grails-app/conf/BootStrap.groovy
+++ Placements/grails-app/conf/BootStrap.groovy
@@ -2,11 +2,11 @@ import placements.Student
 class BootStrap {
 
     def init = { servletContext ->
-	new Student(name:'Zack',coursecode:'G500',notes:'Has dyslexia').save();
+	def zackstu = new Student(name:'Zack',coursecode:'G500',notes:'Has dyslexia').save();
 
-	new Student(name:'Josh',coursecode:'G501',notes:'-').save();
+	def joshstu = new Student(name:'Josh',coursecode:'G501',notes:'-').save();
 
-	new Student(name:'Zeb',coursecode:'G502',notes:'Misbehaved').save();
+	def zebstu = new Student(name:'Zeb',coursecode:'G502',notes:'Misbehaved').save();
 
     }
     def destroy = {
