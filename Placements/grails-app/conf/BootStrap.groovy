441c19771bda520693b3166d9d7401bff3a03a32
Zack Eaton <b001945@my.shu.ac.uk>
2012-05-13 14:10:53 +0100

    Importing data via bootstrap.groovy

--- Placements/grails-app/conf/BootStrap.groovy
+++ Placements/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import placements.Student
 class BootStrap {
 
     def init = { servletContext ->
+	new Student(name:'Zack',coursecode:'G500',notes:'Has dyslexia').save();
+
+	new Student(name:'Josh',coursecode:'G501',notes:'-').save();
+
+	new Student(name:'Zeb',coursecode:'G502',notes:'Misbehaved').save();
+
     }
     def destroy = {
     }
